2d685a1722c6661253673d8497a5a75f0e360cc3
Dmitry Leontev <dmitrii.leontev10200@mail.ru>
2024-10-02 11:17:47 +0500

    Initial Commit

---
+++ Homework_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_1
+//
+//  Created by Дмитрий Леонтьев on 02.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
